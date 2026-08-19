8067dc98ba0011e68bbcadd6110d916b0d17b64a
Tuomas Alarotu <tuomasalarotu2@gmail.com>
2026-08-19 14:13:55 +0300

    test commit

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'c:\Users\tuoma\Desktop\python prac\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\tuoma\Desktop\pythonprac\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
---
+++ .venv/mod1/readme.md
@@ -0,0 +1,9 @@
+# Ohjelmisto 1 - Python harjoitukset
+
+** Tuomas Alarotu **
+
+## Moduuli 1 ja 2
+Tein tehtävät 1 ja 2.
+
+## Moduuli 3
+Tein tehtävät 1, 2
